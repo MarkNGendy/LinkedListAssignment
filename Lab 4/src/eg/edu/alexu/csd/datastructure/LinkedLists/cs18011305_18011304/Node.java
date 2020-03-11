3da61894009dd467f24ef872da6f7ef81cddf391
MarkNGendy <53293337+MarkNGendy@users.noreply.github.com>
2020-03-11 18:00:09 +0200

    Adding Files

---
+++ Lab 4/src/eg/edu/alexu/csd/datastructure/LinkedLists/cs18011305_18011304/Node.java
@@ -0,0 +1,11 @@
+package eg.edu.alexu.csd.datastructure.LinkedLists.cs18011305_18011304;
+
+public class Node {
+    public Node next;
+    public Object value;
+    public Node () {
+        this.next = null;
+        this.value = null;
+    }
+
+}
---
+++ Lab 4/src/eg/edu/alexu/csd/datastructure/LinkedLists/cs18011305_18011304/SingleLinkedList.java
@@ -0,0 +1,63 @@
+package eg.edu.alexu.csd.datastructure.LinkedLists.cs18011305_18011304;
+
+public class SingleLinkedList implements ILinkedList {
+    private Node head;
+    private Node tail;
+    private int size;
+
+    public SingleLinkedList() {
+        head = null;
+        tail = null;
+        size = 0;
+    }
+
+    @Override
+    public void add(int index, Object element) {
+
+    }
+
+    @Override
+    public void add(Object element) {
+
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public void set(int index, Object element) {
+
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void remove(int index) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public ILinkedList sublist(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+}

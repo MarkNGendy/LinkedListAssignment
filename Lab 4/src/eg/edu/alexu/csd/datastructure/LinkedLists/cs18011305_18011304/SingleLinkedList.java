d83c221142eefac9512decc86f34da68b63193d5
MarkNGendy <53293337+MarkNGendy@users.noreply.github.com>
2020-03-13 14:03:52 +0200

    Finishing Single Linked List

--- Lab 4/src/eg/edu/alexu/csd/datastructure/LinkedLists/cs18011305_18011304/SingleLinkedList.java
+++ Lab 4/src/eg/edu/alexu/csd/datastructure/LinkedLists/cs18011305_18011304/SingleLinkedList.java
@@ -1,63 +1,128 @@
 package eg.edu.alexu.csd.datastructure.LinkedLists.cs18011305_18011304;
 
 public class SingleLinkedList implements ILinkedList {
+    public class Node {
+        public Object value;
+        public Node next = null;
+        public Node(Object element) {
+            this.value = element;
+        }
+    }
+
     private Node head;
-    private Node tail;
     private int size;
 
     public SingleLinkedList() {
-        head = null;
-        tail = null;
-        size = 0;
+        this.size = 0;
     }
 
     @Override
     public void add(int index, Object element) {
-
+        // check index and null
+        Node newNode = new Node(element);
+        if (index == 0) {
+            newNode.next = head;
+            head = newNode;
+        } else {
+            Node i = head;
+            for (int count = 0; count < index - 1; count++) {
+                i = i.next;
+            }
+            newNode.next = i.next;
+            i.next = newNode;
+        }
+        size++;
     }
 
     @Override
     public void add(Object element) {
-
+        add(size, element);
     }
 
     @Override
     public Object get(int index) {
-        return null;
+        // check index
+        Node curr = head;
+        for (int count = 0; count < index; count++) {
+            curr = curr.next;
+        }
+        return curr.value;
     }
 
     @Override
     public void set(int index, Object element) {
-
+        Node j;
+        Node newNode = new Node(element);
+        if (index == 0) {
+            j = head;
+            head = newNode;
+            newNode.next = j.next;
+        } else {
+            Node i = head;
+            for (int count = 1; count < index; count++) {
+                i = i.next;
+            }
+            j = i.next;
+            i.next = newNode;
+            newNode.next = j.next;
+        }
     }
 
     @Override
     public void clear() {
-
+        head = null;
+        size = 0;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
     }
 
     @Override
     public void remove(int index) {
-
+        // validation
+        if (index == 0) {
+            head = head.next;
+        } else {
+            Node prev = head;
+            for (int count = 0; count < index - 1; count++) {
+                prev = prev.next;
+            }
+            Node nodeToRemove = prev.next;
+            prev.next = nodeToRemove.next;
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public ILinkedList sublist(int fromIndex, int toIndex) {
-        return null;
+        int size = fromIndex - toIndex + 1;
+        Node i = head;
+        for (int j = 0; j < fromIndex; j++) {
+            i = i.next;
+        }
+        ILinkedList sublist = new SingleLinkedList();
+        for (int j = 0; j < size; j++) {
+            sublist.add(i);
+            i = i.next;
+        }
+        return sublist;
     }
 
     @Override
     public boolean contains(Object o) {
+        Node i = head;
+        while (i != null) {
+            if (i.value.equals(o)) {
+                return true;
+            }
+            i = i.next;
+        }
         return false;
     }
 }

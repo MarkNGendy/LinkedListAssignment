3e3b2d08739bb6ed87281dfe625ea53756d5f35d
MarkNGendy <53293337+MarkNGendy@users.noreply.github.com>
2020-03-14 15:24:47 +0200

    Adding Polynomial Solver classes

--- Lab 4/src/eg/edu/alexu/csd/datastructure/LinkedLists/cs18011305_18011304/Main.java
+++
@@ -1,8 +0,0 @@
-package eg.edu.alexu.csd.datastructure.LinkedLists.cs18011305_18011304;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ Lab 4/src/eg/edu/alexu/csd/datastructure/LinkedLists/cs18011305_18011304/PolynomialSolver.java
@@ -0,0 +1,38 @@
+package eg.edu.alexu.csd.datastructure.LinkedLists.cs18011305_18011304;
+
+public class PolynomialSolver extends SingleLinkedList implements IPolynomialSolver {
+    @Override
+    public void setPolynomial(char poly, int[][] terms) {
+
+    }
+
+    @Override
+    public String print(char poly) {
+        return null;
+    }
+
+    @Override
+    public void clearPolynomial(char poly) {
+
+    }
+
+    @Override
+    public float evaluatePolynomial(char poly, float value) {
+        return 0;
+    }
+
+    @Override
+    public int[][] add(char poly1, char poly2) {
+        return new int[0][];
+    }
+
+    @Override
+    public int[][] subtract(char poly1, char poly2) {
+        return new int[0][];
+    }
+
+    @Override
+    public int[][] multiply(char poly1, char poly2) {
+        return new int[0][];
+    }
+}
